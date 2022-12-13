bdcc70dce453a5b049be48a0879a9a61515518cf
caiooaragao <97796450+caiooaragao@users.noreply.github.com>
2022-12-13 18:56:26 -0300

    third commit

--- functions.py
+++ functions.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from random import randint
 import time
+from bs4 import BeautifulSoup
+import requests
+from termcolor import colored
 
 
 class InstaBot:
@@ -13,36 +16,39 @@ class InstaBot:
         self.driver = webdriver.Chrome()
 
     def login(self):
-        # abrir instagram
-        driver = self.driver
-        driver.get('https://www.instagram.com')
-        time.sleep(5)
-        # capturar local e escrever credenciais de login
-        user_element = driver.find_element("xpath",
-                                           "//input[@name='username']")
-        user_element.clear()
-        time.sleep(3)
-        user_element.send_keys(self.username)
-        password_element = driver.find_element("xpath",
-                                               "//input[@name='password']")
-        password_element.clear()
-        password_element.send_keys(self.password)
-        # capturar e clicar login button
-        login_button = driver.find_element("xpath",
-                                           '//*[@id="loginForm"]/div/div[3]/button')
-        login_button.click()
-        time.sleep(10)
-        # sair de pop-ups do instagram
-        agora_nao_button = driver.find_element(
-            By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button')
-        time.sleep(2)
-        agora_nao_button.click()
-        driver.implicitly_wait(10)
-        notificacao = driver.find_element(
-            By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
-        time.sleep(5)
-        notificacao.click()
-        driver.implicitly_wait(3)
+        try:
+            randomNumber = randint(1, 4)
+            # abrir instagram
+            driver = self.driver
+            driver.get('https://www.instagram.com')
+            time.sleep(5)
+            # capturar local e escrever credenciais de login
+            user_element = driver.find_element("xpath",
+                                               "//input[@name='username']")
+            user_element.clear()
+            time.sleep(3)
+            user_element.send_keys(self.username)
+            password_element = driver.find_element("xpath",
+                                                   "//input[@name='password']")
+            password_element.clear()
+            password_element.send_keys(self.password)
+            # capturar e clicar login button
+            login_button = driver.find_element("xpath",
+                                               '//*[@id="loginForm"]/div/div[3]/button')
+            login_button.click()
+            time.sleep(10)
+            # sair de pop-ups do instagram
+            agora_nao_button = driver.find_element(
+                By.XPATH, '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button')
+            time.sleep(2)
+            agora_nao_button.click()
+            time.sleep(10)
+            notificacao = driver.find_element(
+                By.XPATH, "/html/body/div[2]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]")
+            time.sleep(6)
+            notificacao.click()
+        except:
+            driver.close()
 
     def seguir_target(self, nome, numeroDeLikes):
         randomNumber = randint(1, 4)
@@ -77,7 +83,7 @@ class InstaBot:
                     arquivo.write('seguindo o perfil: {}'.format(pic_hrefs[i]))
                     arquivo.close()'''
 
-    def curtir_fotos_porHashTag(self, hashtag):
+    def curtir_fotos_porHashTag(self, hashtag, numeroDeLikes):
         driver = self.driver
         driver.get('https://www.instagram.com/explore/tags/{}/'.format(hashtag))
         time.sleep(5)
@@ -93,28 +99,83 @@ class InstaBot:
         [href for href in pic_hrefs if hashtag in href]
         print(hashtag + ' fotos: ' + str(len(pic_hrefs)))
         time.sleep(5)
+        print('PIC HREFS:    ', pic_hrefs)
         # ir de link por link dando like
-        for i in range(10, 50):
+        for i in range(10, numeroDeLikes+10):
             driver.get(pic_hrefs[i])
             time.sleep(1.5)
             try:
                 driver.find_element(By.CLASS_NAME, '_aamw').click()
+                print(colored('foto curtida', 'green'))
                 time.sleep(2)
+
             except:
                 time.sleep(1)
                 print('could not locate like button :(')
+
+    def scrape_photos(self, target):
+        driver = self.driver
+        driver.get('https://www.instagram.com/{}/'.format(target))
+        time.sleep(2)
+        for i in range(1, 3):
+            # dar scroll 3 vezes nas publicaçoes da hashtag
+            driver.execute_script(
+                'window.scrollTo(0, document.body.scrollHeight);')
+            time.sleep(3)
+        time.sleep(2)
+        images = driver.find_elements(
+            By.TAG_NAME, 'img')
+
+        for image in images:
+            x = image.get_attribute('src')
+            print(x)
+
+        print('='*50)
+        time.sleep(5)
+        print(images)
+
+        print('='*50)
+
+        '''for image in images:
+            image_src = image.get_attribute('href')
+            image_list.append(image_src)
+            time.sleep(0.5)'''
+
+        '''for image in image_list:
+            wget.download(
+                image_list, '/Users/caioaragao/Documents/vscode/python/instaBot/downloaded_images')'''
+
+        print('download concluido')
 
     def cancelar_seguidores():
         with open('arquivo.txt', "r") as arquivo:
             for line in arquivo:
                 perfil = line
 
+    def randomScript(self):
+        # python script to download instagram image
+        ''' ask user to input the instagram post url '''
+        link = input("Enter Instagram Image URL: ")
 
-caiobot = InstaBot('mymeme0x@gmail.com', 'mymeme123!')
-caiobot.login()
-caiobot.seguir_target('mymeme0x', 2)
+        ''' 
+        create a webdriver chrome object by passing the path of "chromedriver.exe" file.(do not include .exe in the path).
+        '''
+        driver = webdriver.Chrome('chromedriver')
 
-while True:
-    sair = input("quer sair?")
-    if sair in 'Ss':
-        break
+        ''' Open the instagram post on your chrome browser'''
+        driver.get(link)
+
+        ''' Fetch the source file of the html page using BeautifulSoup'''
+        soup = BeautifulSoup(driver.page_source, 'lxml')
+
+        ''' Extract the url of the image from the source code'''
+        img = soup.find('img', class_='FFVAD')
+        img_url = img['src']
+
+        '''Download the image via the url using the requests library'''
+        r = requests.get(img_url)
+
+        with open("instagram"+str(time.time())+".png", 'wb') as f:
+            f.write(r.content)
+
+        print('success')
--- main.py
+++ main.py
@@ -1,8 +1,49 @@
+from termcolor import colored
 from functions import *
 
-while True:
-    print("bem vindo ao menu do InstaBot version BETA \n para sair digite 'SAIR' no login ou senha \n por favor digite seu login e senha abaixo:")
+breaker = False
+while breaker == True:
+    print("\n --------------------------------------- \nbem vindo ao menu do InstaBot version BETA \n para sair digite 'X' no login ou senha \n  --------------------------------------- \n por favor digite seu login e senha abaixo:")
     login = input("Login: ")
     senha = input("Senha: ")
-    if login or senha == 'sair' or 'SAIR':
+    if senha in 'Xx':
+        print(colored('Saindo...', 'red'))
         break
+    else:
+        print(colored('testando credenciais de login...', 'yellow'))
+        try:
+            user01 = InstaBot(login, senha)
+            user01.login()
+            print(colored('login realizado com sucesso!', 'green'))
+            time.sleep(1.5)
+        except:
+            print(colored('problema com o login, tente de novo.', 'red'))
+            time.sleep(3)
+        else:
+            while True:
+                menuLogado = input(
+                    "bem vindo ao menu do InstaBot, agora que está logado, escolha uma opçao:  \n digite [1] para curtir fotos de uma hashTag   \n digite [2] para seguir os seguidores de uma conta especifica \n digite [3] para baixar as fotos de um usuario especifico (você precisa o estar seguindo) \n digite [4] para sair")
+                if menuLogado == '4':
+                    print(colored('Saindo...', 'red'))
+                    time.sleep(0.5)
+                    breaker = True
+
+                if menuLogado == '1':
+                    hashtag = input('Digite a hashtag que você quer usar: ')
+                    numeroDeLikes = int(
+                        input('qual numero de likes que voce quer dar? '))
+                    user01.curtir_fotos_porHashTag(hashtag, numeroDeLikes)
+                if menuLogado == '2':
+                    contaEspecifica = input(
+                        'Digite o nome da conta que você quer que tenha os seguidores seguidos')
+                    numeroSeguidores = int(
+                        input('Digite quantas seguidas você quer dar'))
+                    user01.seguir_target(contaEspecifica, numeroSeguidores)
+
+                if menuLogado == '3':
+                    target = input(
+                        'Digite o nome da conta que voce quer que tenha suas fotos baixadas')
+                    user01.scrape_photos(target)
+
+
+'''caiobot = InstaBot('mymeme0x@gmail.com', 'mymeme1234!')'''
